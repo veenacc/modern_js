8e5414d7b5c867c7d06722d8c72d1694c833bdf8
veenacc <veena.chenicheri@gmail.com>
2024-05-24 13:15:15 -0400

    added moment library

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("hello hello js");
 // console.log("yeaayyy")
 // var x =10;
 // console.log(x + 10);
+console.log(moment().calendar());
+var x = window.prompt("enter date");
+console.log(moment().format("l"));
